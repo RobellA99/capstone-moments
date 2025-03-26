f84bdc9d4b4db55d6c9d792fb65eb7b50b2311ef
Robell <robellasfaw@gmail.com>
2025-03-26 17:25:51 +0000

    Add custom route button

---
+++ src/components/CustomRoute/CustomRoute.jsx
@@ -0,0 +1,9 @@
+import "./CustomRoute.scss";
+
+export default function CustomRoute() {
+  return (
+    <div className="custom-route">
+      <button className="custom-route__button">Create Custom Route</button>
+    </div>
+  );
+}
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,14 @@
 import Map from "../../components/Map/Map";
+import CustomRoute from "../../components/CustomRoute/CustomRoute";
 import "./LandingPage.scss";
 
 export default function LandingPage() {
   return (
-    <div className="map">
-      <Map />
+    <div>
+      <div className="map">
+        <Map />
+      </div>
+      <CustomRoute />
     </div>
   );
 }
